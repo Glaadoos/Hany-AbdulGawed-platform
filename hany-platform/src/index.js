9d06b76083bf6e8c321c970d6985784c4abb336b
Abdul ELrahman A.Bahget <62491215+BaHGet@users.noreply.github.com>
2023-07-19 17:32:14 +0300

    change redirect_uri to glople url

--- hany-platform/src/index.js
+++ hany-platform/src/index.js
@@ -5,9 +5,6 @@ import './index.css';
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
-if(window.location.href === "https://bahget.github.io/"){
-  window.location.href ='https://bahget.github.io/Hany-AbdulGawed-platform/';
-}
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
