219bef513b6fc2fe942a9c69fa7b95cb5bee8ba2
Abdul ELrahman A.Bahget <62491215+BaHGet@users.noreply.github.com>
2023-07-19 16:30:34 +0300

    uploading project files

---
+++ hany-platform/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
+import './App.css';
+
+
+function App() {
+  const { loginWithRedirect } = useAuth0();
+  const { logout } = useAuth0();
+  const {user} = useAuth0();
+
+  if(user){
+    console.log(user)
+    return(
+      <div>
+        <button onClick={() => loginWithRedirect()}>Log In</button>
+        <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</button>
+        <div>
+          <img src={user.picture} alt={user.name} />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      </div>
+    );
+  }else{
+    return(
+      <div>
+        <button onClick={() => loginWithRedirect()}>Log In</button>
+        <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ hany-platform/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter} from "react-router-dom";
+import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Auth0Provider
+      domain="dev-ygrskzanm044w3be.us.auth0.com"
+      clientId="vpwQ42rBahM2eVuTCkNFscsYWfnf1Wzo"
+      authorizationParams={{
+        redirect_uri: 'http://localhost:3000/'
+      }}
+      >
+        <App />
+      </Auth0Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+
