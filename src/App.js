d71622e3ca1d5a6d6806d63dbd3fad9341d570b7
Abdul ELrahman A.Bahget <62491215+BaHGet@users.noreply.github.com>
2023-07-23 18:02:41 +0300

    add login & logout tech and fade in loading page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React from 'react';
 // import { useAuth0 } from "@auth0/auth0-react";
 import './App.css';
 import Header from './components/header'
-import masterPhoto from './components/master SVG.png'
+import Main from './components/main'
+import {useAuth0} from "@auth0/auth0-react";
 
 function App() {
-  /* const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect } = useAuth0();
   const { logout } = useAuth0();
-  const {user} = useAuth0(); */
+  const {user} = useAuth0();
 
   return(
     <div>
-      <Header />
-      {<img src={masterPhoto} alt='master'/>}
+      <Header
+        user={user}
+      />
+      <Main />
     </div>
   )
 
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,86 @@
 import React from 'react';
-import logoCroped from './logo croped.png'
+import logoCroped from './photos/logo croped.png'
+import LoginBtn from './login'
+import LogoutBtn from './logout'
 
 
-const Header = () =>{
-
-    const navber_name =['الرئيسية','المحاضرات','مساعدة','حسابي']
-
+const Header = ({user}) =>{
+    const navber_name =[
+        {
+            'id' :0,
+            'EngName' : '#main',
+            'arbicName' : 'الرئيسية'
+        },
+        {
+            'id' :1,
+            'EngName' : '#lectures',
+            'arbicName' : 'المحاضرات'
+        },
+        {
+            'id' :2,
+            'EngName' : '#help',
+            'arbicName' : 'مساعدة'
+        },
+        {
+            'id' :3,
+            'EngName' : '#account',
+            'arbicName' : 'حسابي',
+            'dropDown' : [
+                {
+                    'id' :0,
+                    'EngName' : '#profile',
+                    'arbicName' : 'صفحتي الشخصيه',
+                },
+                {
+                    'id' :1,
+                    'EngName' : '#ass',
+                    'arbicName' : 'الحضور',
+                },
+                {
+                    'id' :3,
+                    'EngName' : '#logout',
+                    'arbicName' : 'تسجيل الخروج',
+                }
+            ]
+        }
+    ]
     return(
         <header>
             <img id='logo' src={logoCroped} alt='logo' />
             <ul className='navbar'>
-                {navber_name.map((name,num) =>{
-                                return(<li key={num}>{name}</li>)
+                {navber_name.map((obj) =>{
+                                if(obj.id ===3){
+                                    if(user){
+                                        return(
+                                            <li className={'item'+obj.id} key={'item'+obj.id}>
+                                                <div className="dropdown">
+                                                    <span className='dropdown-span'>{obj.arbicName}</span>
+                                                    <div className="dropdown-content">
+                                                        {obj.dropDown.map(item=>{
+                                                            if(item.EngName ==='#logout'){
+                                                                return(<LogoutBtn/>)
+                                                            }
+                                                            if(item.EngName ==='#profile'){
+                                                                return(<div className='dropdown-menu-info'>
+                                                                    <img src={user.picture} alt='profile'/>
+                                                                    <h2 className='dropdonwitem-user-name' key={'dropdonwitem'+item.id}>{user.name}</h2>
+                                                                </div>)
+                                                            }
+                                                            else{return(<p className={'dropdonwitem'+item.id} key={'dropdonwitem'+item.id}>{item.arbicName}</p>)}
+                                                        })}
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        )
+                                    }else{
+                                        return(<LoginBtn/>)   
+                                    }
+                                }else{
+                                    return(
+                                        <li className={'item'+obj.id} key={'item'+obj.id}>
+                                            <a href={obj.EngName}>{obj.arbicName}</a>
+                                            </li>)
+                                }
                 })}
             </ul>
         </header>
