4b75e75604f8f768ace6b590d20bdcedcab9f3cb
BaHGet <abosama150@gmail.com>
2023-08-01 02:17:48 +0300

    error fixed

--- src/components/SetPayingSystem.jsx
+++ src/components/SetPayingSystem.jsx
@@ -18,7 +18,7 @@ const SetPayingSystem = ({user, handelUserUpdating, userPayingSystem}) =>{
         handelUserUpdating(value)
     }
     setTimeout(()=>{
-        window.location.href = window.location.origin
+        window.location.href = 'https://bahget.github.io/Hany-AbdulGawed-platform/'
     }, 4000)
   };
 
