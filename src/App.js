1fa1f191f630b647878b857881bd38f53a3aa234
BaHGet <abosama150@gmail.com>
2023-08-05 01:47:23 +0300

    add code files to undone-changes branch to work on later

--- src/App.js
+++ src/App.js
@@ -26,10 +26,11 @@ function App() {
   const [windowWidth, setWindowWidth] = useState(window.outerWidth);
   const [userPayingSystem, setPayingSystem] = useState('');
   const [videoId, setVideoId] = useState('');
+  const [checkUserExist, setCheckUserExist] = useState(false);
   const [userExist, setUserExist] = useState(false);
   const {user} = useAuth0();
 
-  const id=localStorage.getItem("userEmail")
+  
   const name=localStorage.getItem("userName")
   const email=localStorage.getItem("userEmail")
 
@@ -69,16 +70,15 @@ function App() {
     return pass;
   }
   const handelUserCreation = async () =>{   
-    let checkUserExist;
-    if(currentUser.email.length > 0){
-      checkUserExist=await Api.getSpecific(currentUser.email)
-    }
+    setCheckUserExist(await Api.getSpecific(currentUser.email))
+    console.log(checkUserExist, 'requset sended as getSpecific')  
     
-    if(checkUserExist){
-      if(!(checkUserExist.message === undefined)){
-        // Create new user
-        await Api.createUser(id,name,email,userPayingSystem)
-      }
+    
+    if(localStorage.getItem("userPayingSystem") === 'MPS' || localStorage.getItem("userPayingSystem") ==='LPS'){
+      // Create new user
+      await Api.createUser(generateCodes(),name,email,userPayingSystem)
+      console.log(checkUserExist, 'requset sended as createUser')
+      setCheckUserExist(true);
     }  
   }
   const handelUserUpdating = async (value) =>{
