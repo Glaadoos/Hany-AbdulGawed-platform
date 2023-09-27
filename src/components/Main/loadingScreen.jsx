8221f61727a9179fcbb28880f2aae73ccb2957a9
Abdul ELrahman A.Bahget <abosama150@gmail.com>
2023-09-27 22:15:14 +0300

    create loading screen component and its style

---
+++ src/components/Main/loadingScreen.jsx
@@ -0,0 +1,19 @@
+const LoadingScreen = () =>{
+    return(
+        <>
+            <div class="center">
+                <div class="wave"></div>
+                <div class="wave"></div>
+                <div class="wave"></div>
+                <div class="wave"></div>
+                <div class="wave"></div>
+                <div class="wave"></div>
+                <div class="wave"></div>
+                <div class="wave"></div>
+                <div class="wave"></div>
+                <div class="wave"></div>
+            </div>
+        </>
+    );
+}
+export default LoadingScreen;
