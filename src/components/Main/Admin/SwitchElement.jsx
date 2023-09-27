683b048a7f0c903aa67037d6f6d43fd927763c9f
Abdul ELrahman A.Bahget <abosama150@gmail.com>
2023-09-27 22:14:17 +0300

    add & remove not-checked class from two switched values

--- src/components/Main/Admin/SwitchElement.jsx
+++ src/components/Main/Admin/SwitchElement.jsx
@@ -1,19 +1,24 @@
 import Switch from '@mui/material/Switch';
 
 const SwitchDiv = ({setView}) =>{
-
+    const h1_accounts = document.querySelector('#accounts')
+    const h1_codes = document.querySelector('#codes')
     const handleChange = (e) =>{
         if(e.target.checked){
-            setView('codes')
+            setView('codes');
+            h1_accounts.classList.add('not-checked')
+            h1_codes.classList.remove('not-checked')
         }else{
             setView('accounts')
+            h1_accounts.classList.remove('not-checked')
+            h1_codes.classList.add('not-checked')
         }
     }
     return (
         <div style={{display:'flex',flexDirection:'row'}}>
-            <h4>Accounts</h4>
+            <h4 id='accounts'>Accounts</h4>
             <Switch onChange={handleChange} />
-            <h4>Codes</h4>
+            <h4 id='codes' className='not-checked'>Codes</h4>
         </div>
     );
 }
