2329bff7c0a4dd31c4b57d463bb4c17f4ec794cd
Abdul ELrahman A.Bahget <abosama150@gmail.com>
2023-12-14 23:07:03 +0200

    adds the structure of lesson for futer updata

--- src/components/Branches/Dynamics.jsx
+++ src/components/Branches/Dynamics.jsx
@@ -1,4 +1,33 @@
+/* 
+lessons data structure:
 
+    {
+        name:string,
+        order:string,
+        parts:[ //part
+            { 
+                videoTitle:string,
+                duartion:TimeUine,
+                link:string,
+                attachments:[
+                    notebook:string,
+                    homework:string
+                ]
+            },
+            { //Notebook examples 
+                Name:string,
+                link:string
+            }
+        ]
+        exam:[
+            {
+                name:String,
+                link:String
+            }
+        ]
+    }
+
+*/
 
 const Dynamics = () =>{
     return(<h1 style={{textAlign:'center', marginTop:'200px'}}>Coming Soon <br/> Stay tuned</h1>);
