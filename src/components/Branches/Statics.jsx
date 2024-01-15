4db753e9ecf4ca466ce7372f3630c391263f10ec
Abdul ELrahman A.Bahget <abosama150@gmail.com>
2024-01-15 21:34:57 +0200

    edit lesson statcis07 order

--- src/components/Branches/Statics.jsx
+++ src/components/Branches/Statics.jsx
@@ -163,7 +163,7 @@ const Statics = ({setVideoId, user, userPayingSystem, userCodes}) =>{
         },
         {
             "name":"المحاضرة السابعة",
-            "order":"Statics07",
+            "order":"statics07",
             "parts":[
                 {
                     "lessonName":"القوي المتوازية 1",
