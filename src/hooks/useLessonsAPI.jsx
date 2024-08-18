8cd49102dd3961cd5ef7503b9ba52afbd9614ae4
BaHGet <abosama150@gmail.com>
2024-08-19 02:08:15 +0300

    reduce unnecessary api calls in lessons hook

--- src/hooks/useLessonsAPI.jsx
+++ src/hooks/useLessonsAPI.jsx
@@ -1,14 +1,17 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { getAll } from "../API/LessonsAPI"
 
 
 
 export const useLessonsAPI = (branch) => {
     const [lessons, setLessons] = useState([])
-    const getAllLessons = async () => {
-        let res = await getAll(branch).then(data => data)
-        setLessons(res[0].lessons)
-    }
-    getAllLessons();
+    useEffect(() => {
+        if(lessons.length !== 0) return lessons
+        const getAllLessons = async () => {
+            let res = await getAll(branch).then(data => data)
+            setLessons(res.sort((a, b) => a.order.replace(branch, '') - b.order.replace(branch, '')))
+        }
+        getAllLessons();
+    }, [branch])
     return lessons
 }
