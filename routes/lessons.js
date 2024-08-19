9213dc512bb8a0333b71a7e69e413901a90ce73d
BaHGet <abosama150@gmail.com>
2024-08-19 20:47:25 +0300

    edit lesson update method

--- routes/lessons.js
+++ routes/lessons.js
@@ -41,30 +41,20 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.patch("/:branchname/:order", async (req, res) => {
-  const branchName = req.params.branchname;
-  const lessonOrder = req.params.order; // Get the order from the request parameters
-  const data = req.body; // Get the data from the request body
+router.patch("/:branch/:order", async (req, res) => {
+  const branch = req.params.branch;
+  const lessonOrder = req.params.order;
+  const data = req.body;
   try {
-    // Find the lesson by its order
-    const lessonContainer = await LessonContainer.findOne({
-      BranchName: branchName,   
-    });
-    if (!lessonContainer) {
-      return res.status(404).send({ message: "Lesson not found" });
-    }
-    const lesson = lessonContainer.lessons.find(
-      (lesson) => lesson.order === lessonOrder
-    );
+    const lesson = await Lesson.findOne({ branch: branch, order: lessonOrder });
     if (!lesson) {
       return res.status(404).send({ message: "Lesson not found" });
     }
-    // Update the lesson with the new data
     Object.assign(lesson, data);
-    await lessonContainer.save();
+    await lesson.save();
     res.status(200).send({ message: "lesson updated!" });
   } catch (error) {
-    res.status(500).send({ error: error.message }); // Handle any errors
+    res.status(500).send({ error: error.message }); 
   }
 });
 
